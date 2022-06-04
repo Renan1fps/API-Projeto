f9d873eba0ca478318199a1a6f34004729cf973f
Renan <renan.sergipe68@gmail.com>
2022-06-04 11:10:11 -0300

    feat: setup initial

---
+++ blog_API/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using blog_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog_API.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase {
+        private static List<User> userList = new List<User>();
+
+        [HttpPost]
+        public void CreateUser([FromBody] User user) {
+
+            Console.WriteLine(user.Id);
+            userList.Add(user);
+        }
+    }
+}
